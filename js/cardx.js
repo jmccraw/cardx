70cede65292c74e6100343d43c088ea5cd3fe58e
Justin McCraw <jmccraw1@gmail.com>
2015-10-01 23:26:58 -0400

    Added carousel functionality for the testimonials section

--- js/cardx.js
+++ js/cardx.js
@@ -1,17 +1,225 @@
 (function() {
   'use strict';
 
-  var _mobileMenu = document.querySelector('.top-nav__banner');
-  var _hamburger = _mobileMenu.querySelector('.top-nav__close-btn');
+  // utility vars
+  var _w = window;
 
-  // Add hamburger menu toggle to show/hide menu
-  _hamburger.addEventListener('click', function() {
-    if (!_mobileMenu.classList.contains('is-open')) {
-      _mobileMenu.classList.add('is-open');
+  // navigation vars
+  var _topNav = document.querySelector('.top-nav');
+  var _mobileMenu = _topNav.querySelector('.top-nav__banner');
+  var _hamburger = _mobileMenu.querySelector('.top-nav__close-btn');
+  var scroll = {
+    'current': 0,
+    'previous': 0,
+    // false == down; true == up
+    'direction': true
+  };
+
+  // testimonial vars
+  var _testimonial;
+  var _testimonialList;
+  var _testimonialCrumbs;
+  var testimonialAutoAdvance;
+  var touches = {
+    'current': 0,
+    'previous': 0
+  };
+
+  /**
+   * Checks to see if on the index page
+   * @type {Function}
+   * @returns {Boolean}
+   */
+  function checkIfHomepage() {
+    // if there's a hero section, return true
+    if (document.querySelector('main.hero')) {
+      return true;
+    } else {
+      return false;
     }
-    else {
-      _mobileMenu.classList.remove('is-open');
+  }
+
+  /**
+   * Display the next testimonial
+   * @param {Boolean} touched (Optional) If true, stop auto-advancing
+   * @type {Function}
+   */
+  function loadNextTestimonial(touched) {
+    var i = _testimonialList.length - 1;
+    var tag = -1;
+    if (touched) {
+      _w.clearInterval(testimonialAutoAdvance);
     }
-  }, false);
+
+    for (; i >= 0; --i) {
+      var _tl = _testimonialList[i];
+      if (_tl.classList.contains('is-visible') && i !== tag) {
+        _tl.classList.remove('is-visible');
+        _testimonialCrumbs[i].classList.remove('is-active');
+        if (_tl.nextElementSibling && !_tl.nextElementSibling.classList.contains('testimonials__breadcrumb')) {
+          tag = i + 1;
+          _testimonialCrumbs[i].nextElementSibling.classList.add('is-active');
+          _tl.nextElementSibling.classList.add('is-visible');
+        } else {
+          tag = 0;
+          _testimonialCrumbs[0].classList.add('is-active');
+          _testimonialList[0].classList.add('is-visible');
+        }
+      }
+    }
+  }
+
+  /**
+   * Display the previous testimonial
+   * @param {Boolean} touched (Optional) If true, stop auto-advancing
+   * @type {Function}
+   */
+  function loadPreviousTestimonial(touched) {
+    var i = _testimonialList.length - 1;
+    var tag = -1;
+    if (touched) {
+      _w.clearInterval(testimonialAutoAdvance);
+    }
+
+    for (; i >= 0; --i) {
+      var _tl = _testimonialList[i];
+      if (_tl.classList.contains('is-visible') && i !== tag) {
+        _tl.classList.remove('is-visible');
+        _testimonialCrumbs[i].classList.remove('is-active');
+        if (_tl.previousElementSibling && !_tl.previousElementSibling.classList.contains('testimonials__breadcrumb')) {
+          tag = i - 1;
+          _testimonialCrumbs[i].previousElementSibling.classList.add('is-active');
+          _tl.previousElementSibling.classList.add('is-visible');
+        } else {
+          tag = _testimonialList.length - 1;
+          _testimonialCrumbs[tag].classList.add('is-active');
+          _testimonialList[tag].classList.add('is-visible');
+        }
+      }
+    }
+  }
+
+  /**
+   * Load the testimonials section functions and event handlers
+   * @type {Function}
+   */
+  function loadTestimonials() {
+    var touching = false;
+    var setCurrentPosition = throttle(function(e) {
+      if (touching) {
+        touches.current = e.touches[0].clientX;
+      }
+    }, 100);
+    _testimonial= document.querySelector('.testimonials');
+    _testimonialList = _testimonial.querySelectorAll('.testimonials__testimonial');
+    _testimonialCrumbs = _testimonial.querySelectorAll('.testimonials__crumb');
+
+    // attach event handlers to testimonials
+    _testimonial.addEventListener('touchstart', function(e) {
+      touching = true;
+      touches.previous = e.touches[0].clientX;
+    }, false);
+
+    _testimonial.addEventListener('touchmove', setCurrentPosition, false);
+
+    _testimonial.addEventListener('touchend', function() {
+      touching = false;
+      // advancing carousel
+      if (touches.current < touches.previous) {
+        loadNextTestimonial(true);
+      } else if (touches.current > touches.previous) {
+        loadPreviousTestimonial(true);
+      }
+    }, false);
+
+    // auto advance the testimonials section
+    testimonialAutoAdvance = _w.setInterval(loadNextTestimonial, 3000);
+  }
+
+  /**
+   * Add or remove the mobile menu visibility toggle class
+   * @type {Function}
+   */
+  function toggleMobileMenu() {
+    if (!_topNav.classList.contains('is-hidden')) {
+      _topNav.classList.add('is-hidden');
+      // check if the menu is open and close it if so
+      if (_mobileMenu.classList.contains('is-open')) {
+        _mobileMenu.classList.remove('is-open');
+      }
+    } else {
+      _topNav.classList.remove('is-hidden');
+    }
+  }
+
+  /**
+   * Throttles the events being received so it doesn't bog down the user experience
+   * Taken from Remy Sharp's implementation: https://remysharp.com/2010/07/21/throttling-function-calls
+   * @param{Object} fn The function to execute after throttling
+   * @param{Number} threshhold The wait time in milliseconds
+   * @param{Object} scope The context of 'this' or null
+   * @returns {Function}
+   */
+  function throttle(fn, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    var last,
+      deferTimer;
+    return function () {
+      var context = scope || this;
+      var now = +new Date,
+        args = arguments;
+      if (last && now < last + threshhold) {
+        // hold on to it
+        clearTimeout(deferTimer);
+        deferTimer = setTimeout(function () {
+          last = now;
+          fn.apply(context, args);
+        }, threshhold);
+      } else {
+        last = now;
+        fn.apply(context, args);
+      }
+    };
+  }
+
+  /**
+   * Initialization functions
+   * @type {Function}
+   */
+  function init() {
+
+    // if the homepage, start the testimonials carousel
+    if (checkIfHomepage()) {
+      loadTestimonials();
+    }
+
+    /**
+     * Throttle scrolling to check if mobile nav should recede
+     * @type {Function}
+     */
+    var testMobileNav = throttle(function() {
+      scroll.current = _w.scrollY;
+      if (scroll.current > scroll.previous + 100 && scroll.direction) {
+        scroll.direction = false;
+        toggleMobileMenu();
+      } else if (scroll.current < scroll.previous - 50 && !scroll.direction) {
+        scroll.direction = true;
+        toggleMobileMenu();
+      }
+      scroll.previous = scroll.current;
+    }, 500);
+    document.addEventListener('scroll', testMobileNav, false);
+
+    // Add hamburger menu toggle to show/hide menu
+    _hamburger.addEventListener('click', function () {
+      if (!_mobileMenu.classList.contains('is-open')) {
+        _mobileMenu.classList.add('is-open');
+      } else {
+        _mobileMenu.classList.remove('is-open');
+      }
+    }, false);
+  }
+
+  init();
 
 })();
